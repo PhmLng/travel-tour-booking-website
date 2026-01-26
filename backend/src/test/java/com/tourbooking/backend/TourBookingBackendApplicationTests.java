1f40f867c832109ba237bd80ba913d05355e1f98
PhmLng <phamdacluong04@gmail.com>
2026-01-26 14:12:47 +0700

    add backend

---
+++ backend/src/test/java/com/tourbooking/backend/TourBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourbooking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ea7af2debb68e250b2c6b7371a16c46c30b4b9b8
PhmLng <phamdacluong04@gmail.com>
2026-03-16 14:39:11 +0700

    Passenger Api

--- backend/src/main/java/com/tourbooking/backend/mapper/PassengerMapper.java
+++ backend/src/main/java/com/tourbooking/backend/mapper/PassengerMapper.java
@@ -1,6 +1,7 @@
 package com.tourbooking.backend.mapper;
 
 import com.tourbooking.backend.dto.passenger.PassengerRequest;
+import com.tourbooking.backend.dto.passenger.PassengerResponse;
 import com.tourbooking.backend.entity.Passenger;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -8,4 +9,5 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PassengerMapper {
     public Passenger toPassenger (PassengerRequest passengerRequest);
+    public PassengerResponse toPassengerResponse (Passenger passenger);
 }
--- backend/src/main/java/com/tourbooking/backend/service/PassengerService.java
+++ backend/src/main/java/com/tourbooking/backend/service/PassengerService.java
@@ -5,7 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PassengerService {
-    public Page<PassengerResponse> getAllPassengers(Pageable pageable);
-    public PassengerResponse getPassengerById(int id);
+    public Page<PassengerResponse> getAllPassengers(int page, int size);
+    public PassengerResponse getPassengerById(Long id);
     public void deletePassengerById(int id);
 }

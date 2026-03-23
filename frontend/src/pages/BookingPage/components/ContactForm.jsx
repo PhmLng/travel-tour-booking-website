8e7d5d12f937da433e12c36c16be86e6ce73e77b
TuanAnhHandsome <vut.anh1603@gmail.com>
2026-03-23 22:19:42 +0700

    Delete checkbox phong don

--- frontend/src/pages/BookingPage/components/ContactForm.jsx
+++ frontend/src/pages/BookingPage/components/ContactForm.jsx
@@ -93,16 +93,6 @@ const ContactForm = ({ contact, onChange, errors, currentUser }) => {
           </select>
         </div>
 
-        <div className="form-group form-group-checkbox">
-          <label className="checkbox-label">
-            <input
-              type="checkbox"
-              checked={!!contact.singleRoom}
-              onChange={(e) => onChange("singleRoom", e.target.checked)}
-            />
-            <span>Yêu cầu phòng đơn</span>
-          </label>
-        </div>
       </div>
     </section>
   );
